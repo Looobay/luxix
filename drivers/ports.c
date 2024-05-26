59ff207d769a2ceebd841510d69807173fcbe587
Luc <lucrobertvillanueva@gmail.com>
2024-05-26 10:44:28 +0200

    color !

---
+++ drivers/ports.c
@@ -0,0 +1,47 @@
+/*
+ * Lis un octet sur le port spécifié et renvoie le resultat
+ * Read a byte from the specified port and return the result
+ */
+unsigned char port_byte_in (unsigned short port) {
+    unsigned char result;
+    /* Inline assembler syntax
+     * !! Notice how the source and destination registers are switched from NASM !!
+     *
+     * '"=a" (result)'; result = EAX ; il défini la variable result sur EAX (AL)
+     * '"d" (port)': défini EDX (DX) sur la variable port
+     * 
+     * DONC ON DIS A L'ASSEMBLEUR QUE L'ON VEUT LIRE L'OCTET DEFINI DANS PORT ET ON RETOURNE SA VALEUR DANS RESULT
+     * 
+     * Les inputs et les outputs sont séparers par ":""
+     */
+    __asm__("in %%dx, %%al" : "=a" (result) : "d" (port));
+    return result;
+}
+/*
+* Envoie l'octet spécifié (data) dans le port spécifié (port).
+* Sends the specified byte (data) to the specified port (port).
+*/
+void port_byte_out (unsigned short port, unsigned char data) {
+    /* Notice how here both registers are mapped to C variables and
+     * nothing is returned, thus, no equals '=' in the asm syntax 
+     * However we see a comma since there are two variables in the input area
+     * and none in the 'return' area
+     */
+    __asm__("out %%al, %%dx" : : "a" (data), "d" (port));
+}
+/*
+* Permet d'enregistrer un mot correctement dans DX et AX qui sont des registres pour les mots !
+* Allows you to record a word correctly in DX and AX which are registers for words!
+*/
+unsigned short port_word_in (unsigned short port) {
+    unsigned short result;
+    __asm__("in %%dx, %%ax" : "=a" (result) : "d" (port));
+    return result;
+}
+/*
+* Envoie le mot spécifié (data) dans le port spécifié (port).
+* Sends the specified word (data) to the specified port (port).
+*/
+void port_word_out (unsigned short port, unsigned short data) {
+    __asm__("out %%ax, %%dx" : : "a" (data), "d" (port));
+}
--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,4 +1,18 @@
+#include "../drivers/ports.h"
+
 void main() {
-    char* video_memory = (char*) 0xb8000;
-    *video_memory = 'X';
+    // on envoie la position du curseur dans 
+    port_byte_out(0x3d4, 14);
+
+    int position = port_byte_in(0x3d5); // la position équivaut donc à la valeure de l'octet 0x3d5
+    position = position << 8;
+
+    port_byte_out(0x3d4,15);
+    position += port_byte_in(0x3d5); // on aditionne la position avec la valeure de l'octet 0x3d5
+
+    int offset_from_vga = position * 2;
+
+    char *vga = 0xb8000;
+    vga[offset_from_vga] = 'X';
+    vga[offset_from_vga+1] = 0x0f; //couleur du charactère 0xAB (A = couleur de fond ; B = couleur de la lettre) / comme sur Windows
 }
